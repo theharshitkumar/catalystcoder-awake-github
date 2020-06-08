7b63e584030b0fc8d706a74d026c9db91db70c03
theharshitkumar <harshit.hk.3012@gmail.com>
2020-06-08 23:13:32 +0530

    initial commit

---
+++ components/SiteFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="site-footer">
+    {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - All
+    Rights Reserved
+    <br />
+   <a href="https://www.linkedin.com/in/harshitkumar197/"> Made with <span style="color: #e25555;">&#9829;</span> By HK </a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter'
+}
+</script>
+
+<style scoped>
+footer {
+  background: #eee;
+  padding: 20px 10px;
+  text-align: center;
+  transition: 0.5s ease all;
+}
+</style>
